04d1588899372020b7cd1e43f7b684f3b5e88e1f
Torben Haack <haack.t@icloud.com>
2022-08-26 10:21:36 +0200

    fix: :fire: Remove lineClamp Plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,13 @@ import sitemap from "@astrojs/sitemap";
 import remarkGemoji from "remark-gemoji";
 import remarkStringify from "remark-stringify/lib/index.js";
 import remarkReadingTime from "./src/remark/remark-reading-time.mjs";
-import remarkLineClamp from "./src/remark/remark-line-clamp.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://haackt.com/",
   integrations: [
     mdx({
-      remarkPlugins: [
-        remarkGemoji,
-        remarkStringify,
-        remarkReadingTime,
-        remarkLineClamp,
-      ],
+      remarkPlugins: [remarkGemoji, remarkStringify, remarkReadingTime],
     }),
     tailwind(),
     sitemap(),
