5e7b78a7bafcc62ee8b307bb4625b8f2337bb318
Torben Haack <haack.t@icloud.com>
2022-08-26 11:22:48 +0200

    feat: :construction: MD Editor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import remarkGemoji from "remark-gemoji";
 import remarkStringify from "remark-stringify/lib/index.js";
 import remarkReadingTime from "./src/remark/remark-reading-time.mjs";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +17,7 @@ export default defineConfig({
     }),
     tailwind(),
     sitemap(),
+    react(),
   ],
   vite: {
     ssr: {
