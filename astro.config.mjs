c6345a4f58283a33a7cd485b69c5939f3a544cdb
Torben Haack <haack.t@icloud.com>
2022-08-29 11:20:48 +0200

    feat: :heavy_plus_sign: Add Vercel Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import remarkGemoji from "remark-gemoji";
 import remarkStringify from "remark-stringify/lib/index.js";
 import remarkReadingTime from "./src/remark/remark-reading-time.mjs";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://haackt.com/",
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkGemoji, remarkStringify, remarkReadingTime],
-    }),
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [mdx({
+    remarkPlugins: [remarkGemoji, remarkStringify, remarkReadingTime]
+  }), tailwind(), sitemap()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  output: "server",
+  adapter: vercel()
 });
